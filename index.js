2814d11bf87801429628bca3c6c6eb322774e697
Mat Warger <mwarger@gmail.com>
2020-06-18 18:33:05 -0500

    cookie experiment

--- index.js
+++ index.js
@@ -1,16 +1,64 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-express');
+const express = require('express');
+
 const SessionAPI = require('./datasources/sessions');
 const SpeakerAPI = require('./datasources/speakers');
 
 const typeDefs = require('./schema.js');
 const resolvers = require('./resolvers/index');
 
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+const getUser = (token) => {
+  jwt.verify(token)
+  return '381b010e-f51d-4fca-a249-271f72a6a5b9';
+};
+
 const dataSources = () => ({
   sessionAPI: new SessionAPI(),
   speakerAPI: new SpeakerAPI(),
 });
-const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources,
+  context: ({ req }) => {
+    const token = req.headers.authorization || '';
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`graphQL running at ${url}`);
+    // try to retrieve a user with the token
+    const user = getUser(token);
+
+    // optionally block the user
+    // we could also check user roles/permissions here
+    if (!user) throw new AuthenticationError('you must be logged in');
+
+    // add the user to the context
+    return { user };
+  },
 });
+
+const app = express()
+
+// server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+//   console.log(`graphQL running at ${url}`);
+// });
+
+var corsOptions = {
+  origin: process.env.FRONTEND_URL,
+  credentials: true // <-- REQUIRED backend setting
+};
+
+app.use(cors(corsOptions));
+app.use(cookieParser());
+
+server.applyMiddleware({
+  app,
+  path: '/',
+  cors: false, // disables the apollo-server-express cors to allow the cors middleware use
+})
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
--- resolvers/sessions.js
+++ resolvers/sessions.js
@@ -3,7 +3,7 @@ const _ = require('lodash');
 module.exports = {
   async speakers(session, args, { dataSources }) {
     const speakers = await dataSources.speakerAPI.getSpeakers();
-
+    console.log('here');
     const returns = speakers.filter((speaker) => {
       return _.filter(session.speakers, { id: speaker.id }).length > 0;
     });
